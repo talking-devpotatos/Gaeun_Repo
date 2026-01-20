d2f820a58ced3a74616cfbe27f4196fac84e8e7d
Gaeun <zzazaza74@gmail.com>
2026-01-21 04:09:32 +0900

    second

---
+++ gaeun_java/src/main/java/org/example/ch1/Settings.java
@@ -0,0 +1,22 @@
+package org.example.ch1;
+
+public class Settings
+{
+    public static void main(String[] args)
+    {
+        //단축키
+
+        // 자동완성 => Ctrl + Space
+        // 줄 복사 => Ctrl + d
+        // 줄 삭제 => Ctrl + y
+        // 전체 선택 => Ctrl + a
+        // 줄 이동 => shift + alt + 방향키
+        // 들여쓰기/내어쓰기 => tab / shift + tab
+        // 코드 포맷 정리 => Ctrl + alt + l
+        // 자동 임포트 + 정리 => Ctrl + alt + o
+        // 실행 => Shift + f10
+        // 프로젝트 구조 보기 => alt + 1
+        // 중단 => ctrl + f2
+
+    }
+}
